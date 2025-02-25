601f38c8f8d9296d6edf3ee52d98760f3d82cc52
mrfluffy <zastian@mrfluffy.xyz>
2025-02-25 12:49:17 +0000

    moved env vard to zsh

--- universal/dots/zsh/zsh.nix
+++ universal/dots/zsh/zsh.nix
@@ -22,6 +22,20 @@
     sessionVariables = {
       HISTSIZE = 3000;
       SAVEHIST = 3000;
+      CARGO_HOME = "$XDG_DATA_HOME/cargo";
+      GNUPGHOME = "$XDG_DATA_HOME/gnupg";
+      GOPATH = "$XDG_DATA_HOME/go";
+      GRADLE_USER_HOME = "$XDG_DATA_HOME/gradle";
+      IPYTHONDIR = "$XDG_CONFIG_HOMEipython";
+      JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
+      LESSHISTFILE = "$XDG_CACHE_HOME/less/history";
+      NUGET_PACKAGES = "$XDG_CACHE_HOME/NuGetPackages";
+      PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
+      KERAS_HOME = "$XDG_STATE_HOME/keras";
+      RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
+      XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose";
+      SSB_HOME = "$XDG_DATA_HOME/zoom";
+      HISTFILE = "$XDG_STATE_HOME/zsh/history";
     };
     shellAliases = {
       nix-switch = "sudo nixos-rebuild switch";
